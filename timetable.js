069c2bb9a96ed1d1082514543ae5f727ba42883f
Anza-arain <anzaarain125@gmail.com>
2025-03-29 10:52:18 +0500

    Add files via upload

---
+++ timetable.js
@@ -0,0 +1,108 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'CSE room no. 2',
+        subject: 'CN',
+        type: 'Test'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'CSE room no. 2',
+        subject: 'SP',
+        type: 'Assignment'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'LAB',
+        subject: 'WE',
+        type: 'Presentation'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'CSE room no. 2',
+        subject: 'SE',
+        type: 'Test'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'CSE room no. 2',
+        subject: 'WE',
+        type: 'Quiz'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'LAB',
+        subject: 'CN',
+        type: 'Test'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: 'CSE room no. 2',
+        subject: 'MIT',
+        type: 'Test'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: 'CSE room no. 2',
+        subject: 'SP',
+        type: 'Test'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '11-12 AM',
+        roomNumber: 'CSE room no. 2',
+        subject: 'MIT',
+        type: 'Assignment'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: 'CSE room no. 2',
+        subject: 'CN',
+        type: 'Presentation'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'CSE room no. 2',
+        subject: 'WE',
+        type: 'Test'
+    }
+]
+
+const Friday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: 'CSE room no. 2',
+        subject: 'SE',
+        type: 'Test'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'CSE room no. 2',
+        subject: 'SP',
+        type: 'Quiz'
+    },
+]
+
+const Saturday =[
+    {   
+        time: 'Saturday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
